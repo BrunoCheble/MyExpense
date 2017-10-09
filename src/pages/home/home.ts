12c009b2416472952629c81de590c8f364724652
BrunoCheble <contato.cheble@gmail.com>
2017-10-09 15:54:49 -0300

    Alteração de Status e ação de deletar
    Alteração de status do status;
    Ação de deletar, se tiver pendente é para excluir todas as pendentes;
    caso seja uma paga, deletar apenas a despesa respectiva.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { Http, URLSearchParams } from '@angular/http';
+import { Http, URLSearchParams, Headers } from '@angular/http';
 
 @Component({
   selector: 'page-home',
@@ -10,8 +10,10 @@ export class HomePage {
 	
 	public filter: string = new Date().toISOString();
 	private _now: string = new Date().toISOString();
-	
+	private _Url = 'http://localhost/WSMyExpense/web/expenses';
+
 	public items;
+	public total_expense;
 
   	constructor(public navCtrl: NavController, private _http: Http) {
   		this.listExpenses();
@@ -23,32 +25,78 @@ export class HomePage {
   		_search.set('ExpenseSearch[due_date]', this.filter);
 
   		this._http
-	    .get('http://localhost/lembrete/web/expenses',{ search : _search })
+	    .get(this._Url,{ search : _search })
 	    .map(res => res.json())
 	    .toPromise()
 	    .then(
-	    	expenses => {this.items = expenses},
+	    	expenses => {
+	    		this.items = expenses;
+	    		this.setTotalExpense();
+	    	},
 	    	err => {console.log(err)}
     	);
   	}
-  	/*
-  	public getTotal() {
-  		
-  		let total: number = 0;
+
+  	public setTotalExpense() {
+
+  		let total_expense : number = 0;
 
   		this.items.forEach(function(item){
   			if(item.status != 'PAGO')
-  				total += Number(item.value);
+  				total_expense += Number(item.value);
   		});
 
-  		return total;
-  	}*/
+  		this.total_expense = total_expense;
+  	}
 
   	public changeFilter() {
   		this.listExpenses();
   	}
 
-  	public itemSelected(item) {
+  	public editItem(item) {
   		console.log(item);
+  	}
+
+  	public deleteItem(item) {
+  		
+  		let _headers = new Headers({'Content-Type': 'application/x-www-form-urlencoded'});
+  		
+  		this._http
+	    .delete(`${this._Url}/${item.id}`, {headers : _headers})
+	    .map(res => res.json())
+	    .toPromise()
+	    .then(
+	    	expense => { this.listExpenses(); },
+	    	err => { console.log(err) }
+    	);
+
+  	}
+
+  	public updateStatusItem(item) {
+
+  		let _headers = new Headers({'Content-Type': 'application/x-www-form-urlencoded'});
+  		let _params  = new URLSearchParams(); 		
+  		let status   = 'PENDENTE';
+
+  		if(item.status == 'PENDENTE') {  
+  			status = 'PAGO';
+  		}
+
+		_params.set('Expense[status]', status);
+ 		
+  		this._http
+	    .put(`${this._Url}/${item.id}`, _params.toString(), {headers : _headers})
+	    .map(res => res.json())
+	    .toPromise()
+	    .then(
+	    	expense => {
+	    		item.status = status;
+	    		this.setTotalExpense();
+	    	},
+	    	err => {
+	    		console.log(err)
+	    	}
+    	);
+
   	}
 }
